e75751448710de23d7855fa00e94368f3f991428
Amber3378 <92365929+Amber3378@users.noreply.github.com>
2022-04-05 16:02:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function go()
+{
+    window.location = "https://www.amazon.com/Chronicles-Narnia-8-Book-Trivia-Book/dp/0062690574/ref=sr_1_13?crid=36RCI3XU3IS7Z&keywords=narnia+collection&qid=1649136539&sprefix=narnia+collectio%2Caps%2C436&sr=8-13";
+}
